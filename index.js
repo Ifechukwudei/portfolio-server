75cdaab778d688bf6f3d302f5c71a8e13109ad56
ifechukwudei <nwokoifechukwu@gmail.com>
2022-10-26 19:49:07 +0100

    add new features

---
+++ index.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(
+    __dirname,
+    'portfolio-server',
+    req.url === '/' ? 'index.html' : req.url
+  )
+  let contentType = getContentType(filePath) || 'text/html'
+  let emptyPagePath = path.join(__dirname, 'portfolio-server', '404.html')
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(emptyPagePath, 'utf8', (err, data) => {
+          res.writeHead(200, { 'content-Type': contentType })
+          res.end(data)
+        })
+      } else {
+        res.writeHead(500)
+        res.end('A server error has occured')
+      }
+    }
+    if (!err) {
+      res.writeHead(200, { 'content-Type': contentType })
+      res.end(data)
+    }
+  })
+})
+
+const getContentType = (filePath) => {
+  let extname = path.extname(filePath)
+  if (extname === '.js') {
+    return 'text/javascript'
+  }
+  if (extname === '.css') {
+    return 'text/css'
+  }
+  if (extname === '.jpg') {
+    return 'image/jpg '
+  }
+  if (extname === '.png') {
+    return 'image/png'
+  }
+}
+const port = 3000
+
+server.listen(port, () => {
+  console.log(`sever is running on port ${port}`)
+})

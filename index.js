617a34776a90b13d42c6946ef0742472076e697f
ifechukwudei <nwokoifechukwu@gmail.com>
2022-10-26 22:39:05 +0100

    new changes

--- index.js
+++ index.js
@@ -3,37 +3,25 @@ const fs = require('fs')
 const http = require('http')
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    let filePath = path.join(__dirname, 'public', 'index.html')
+    let filePath = path.join(__dirname, 'portfoilo-server', 'index.html')
     fs.readFile(filePath, 'utf8', (err, data) => {
       res.writeHead(200, { 'content-Type': 'text.html' })
       res.end(data)
     })
   }
   if (req.url === '/contact') {
-    let filePath = path.join(__dirname, 'public', 'contact.html')
+    let filePath = path.join(__dirname, 'portfoilo-server', 'contact.html')
     fs.readFile(filePath, 'utf8', (err, data) => {
       res.writeHead(200, { 'content-Type': 'text.html' })
       res.end(data)
     })
   }
   if (req.url === '/about') {
-    let filePath = path.join(__dirname, 'public', 'about.html')
+    let filePath = path.join(__dirname, 'portfoilo-server', 'about.html')
     fs.readFile(filePath, 'utf8', (err, data) => {
       res.writeHead(200, { 'content-Type': 'text.html' })
       res.end(data)
     })
-  }
-
-  if (err) {
-    if (err.code === 'ENOENT') {
-      fs.readFile(emptyPagePath, 'utf8', (err, data) => {
-        res.writeHead(200, { 'content-Type': contentType })
-        res.end(data)
-      })
-    } else {
-      res.writeHead(500)
-      res.end('A server error has occured')
-    }
   }
 })
 

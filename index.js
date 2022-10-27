cca0983bbbc6e951764f6e4a66cd117b311f44eb
ifechukwudei <nwokoifechukwu@gmail.com>
2022-10-27 11:32:48 +0100

    server correction

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 const path = require('path')
 const fs = require('fs')
 const http = require('http')
+
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    let filePath = path.join(__dirname, 'portfoilo-server', 'index.html')
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      res.writeHead(200, { 'content-Type': 'text.html' })
-      res.end(data)
-    })
-  }
-  if (req.url === '/contact') {
-    let filePath = path.join(__dirname, 'portfoilo-server', 'contact.html')
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      res.writeHead(200, { 'content-Type': 'text.html' })
-      res.end(data)
-    })
-  }
-  if (req.url === '/about') {
-    let filePath = path.join(__dirname, 'portfoilo-server', 'about.html')
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      res.writeHead(200, { 'content-Type': 'text.html' })
-      res.end(data)
-    })
-  }
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url)
+  let contentType = getContentType(filePath) || 'text/html'
+  let emptyPagePath = path.join(__dirname, '', '404.html')
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.readFile(emptyPagePath, 'utf8', (err, data) => {
+          res.writeHead(200, { 'content-Type': contentType })
+          res.end(content)
+        })
+      } else {
+        res.writeHead(500)
+        res.end('A server error has occured')
+      }
+    }
+    if (!err) {
+      res.writeHead(200, { 'content-Type': contentType })
+      res.end(content)
+    }
+  })
 })
 
+const getContentType = (filePath) => {
+  let extname = path.extname(filePath)
+  if (extname === '.js') {
+    return 'text/javascript'
+  }
+  if (extname === '.css') {
+    return 'text/css'
+  }
+  if (extname === '.jpg') {
+    return 'image/jpg '
+  }
+  if (extname === '.png') {
+    return 'image/png'
+  }
+}
 const port = 3000
 
 server.listen(port, () => {
